8ae038189926754c05c0b35f4e569a63378b48d8
m-grande <michelegrande@gmail.com>
2024-11-20 16:31:02 +0100

    Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,4 +10,26 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(URL, json=body, headers=headers)
 
-    return json.loads(response.text)
+    response_dict = json.loads(response.text)
+
+    emotion_data = response_dict["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotion_data["anger"]
+    disgust_score = emotion_data["disgust"]
+    fear_score = emotion_data["fear"]
+    joy_score = emotion_data["joy"]
+    sadness_score = emotion_data["sadness"]
+
+    scores = {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+    }
+
+    dominant_emotion = max(scores, key=scores.get)
+
+    scores["dominant_emotion"] = dominant_emotion
+
+    return scores
